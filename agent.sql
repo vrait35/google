ec51ad0a29d16eb04e07eb9b8b31b1fc2edbae26
vrait35 <40663872+vrait35@users.noreply.github.com>
2018-08-20 18:47:13 +0600

    Create agent.sql

---
+++ agent.sql
@@ -0,0 +1,24 @@
+# google
+
+
+--База данных db_anuar_agent, 
+--server: A-306-04
+--Задания: anuar_finish
+
+
+-- Делаем резервную копию , название файла  с датой её создания
+declare @a varchar(25)
+set @a='C:\asd\ti'+CONVERT(varchar(15),CONVERT(date, GETDATE()))+'.bak'
+BACKUP DATABASE db_anuar_agent
+TO DISK = @a
+
+--Проверяем файл резервной копии,не восстановляя её
+declare @q varchar(25)
+set @q='C:\asd\ti'+CONVERT(varchar(15),CONVERT(date, GETDATE()))+'.bak'
+RESTORE VERIFYONLY  
+FROM DISK=@q
+
+----Переносим таблицу в файл с текущей датой _^^_  %date%
+EXEC xp_cmdshell   'bcp db_anuar_agent.dbo.aa out c:\asd\qrd%date%.xls -c  -C 1251 -S A-306-04 -U anuar_1 -P anuar_2' 
+
+
